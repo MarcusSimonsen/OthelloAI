3b634cc5ba26164fa027451781fa19613fc32c71
Marcus Henrik Simonsen <marcus@itis.dk>
2024-03-05 11:54:57 +0100

    Print move times

--- Test.java
+++ Test.java
@@ -8,6 +8,12 @@ public class Test {
         IOthelloAI ai2 = null;
         int ai1Wins = 0;
         int ai2Wins = 0;
+        long ai1TotalTime = 0;
+        long ai2TotalTime = 0;
+        long ai1LongestTime = 0;
+        long ai2LongestTime = 0;
+        int ai1Moves = 0;
+        int ai2Moves = 0;
         int draws = 0;
         int size = 8;
         String errMsg = null;
@@ -47,7 +53,7 @@ public class Test {
             return;
         }
 
-        for (int i = 0; i < testAmount / 2; i++) {
+        for (int i = 0; i < testAmount; i++) {
             System.out.println(i);
             GameState s = new GameState(size, 1);
             while (!s.isFinished()) {
@@ -55,7 +61,20 @@ public class Test {
                     s.changePlayer();
                     continue;
                 }
-                if (!s.insertToken(getPlaceForNextToken(s, ai1, ai2))) {
+                long startTime = System.nanoTime();
+                Position move = getPlaceForNextToken(s, ai1, ai2);
+                long duration = System.nanoTime() - startTime;
+                if (s.getPlayerInTurn() == 1) {
+                    ai1TotalTime += duration;
+                    ai1LongestTime = Math.max(ai1LongestTime, duration);
+                    ai1Moves++;
+                }
+                else {
+                    ai2TotalTime += duration;
+                    ai2LongestTime = Math.max(ai2LongestTime, duration);
+                    ai2Moves++;
+                }
+                if (!s.insertToken(move)) {
                     if (s.getPlayerInTurn() == 1)
                         System.out.println("AI1 made an illegal move");
                     else
@@ -70,33 +89,14 @@ public class Test {
             else
                 draws++;
         }
-        for (int i = 0; i < testAmount / 2; i++) {
-            System.out.println(i + testAmount / 2);
-            GameState s = new GameState(size, 1);
-            while (!s.isFinished()) {
-                if (s.legalMoves().size() == 0) {
-                    s.changePlayer();
-                    continue;
-                }
-                if (!s.insertToken(getPlaceForNextToken(s, ai2, ai1))) {
-                    if (s.getPlayerInTurn() == 2)
-                        System.out.println("AI1 made an illegal move");
-                    else
-                        System.out.println("AI2 made an illegal move");
-                }
-            }
-            int[] tokens = s.countTokens();
-            if (tokens[0] > tokens[1])
-                ai2Wins++;
-            else if (tokens[1] > tokens[0])
-                ai1Wins++;
-            else
-                draws++;
-        }
 
         System.out.println("AI1 got " + ai1Wins + " wins");
         System.out.println("AI2 got " + ai2Wins + " wins");
         System.out.println(draws + " draws");
+        System.out.println("AI1 used an average of " + (ai1TotalTime / 1_000_000 / ai1Moves) + " milliseconds per move");
+        System.out.println("AI2 used an average of " + (ai2TotalTime / 1_000_000 / ai2Moves) + " milliseconds per move");
+        System.out.println("AI1s slowest move took " + (ai1LongestTime / 1_000_000) + " milliseconds");
+        System.out.println("AI2s slowest move took " + (ai2LongestTime / 1_000_000) + " milliseconds");
     }
 
     /**
